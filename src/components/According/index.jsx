02b213072395b79d3bd74d8bbe1bbd28d34aed64
Nchonganyi-Ernesto <nchonganyiernesto4@gmail.com>
2026-07-07 08:44:47 -0700

    completed accordion task

--- src/components/According/index.jsx
+++ src/components/According/index.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
 import data from "./data";
+import "./styles.css";
 
 export default function According(){
     const [selected, setSelected] = useState(null);
 
     function handleSingleSelection(getCurrentId){
-       console.log(getCurrentId)
+        setSelected((prevSelected) => (prevSelected === getCurrentId ? null : getCurrentId));
     }
 
     return (
@@ -13,18 +14,24 @@ export default function According(){
         <div className="accordian">
             {
                 data && data.length > 0 ?
-                data.map(dataItem=> <div className="Item">
-                    <div onClick={()=>handleSingleSelection(dataItem.id)} className="title">
-                        <h3>{dataItem.title}</h3>
-                        <span>+</span>
-
+                data.map((dataItem) => (
+                    <div key={dataItem.id} className="Item">
+                        <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
+                            <h3>{dataItem.title}</h3>
+                            <span>+</span>
+                        </div>
+                        {
+                            selected === dataItem.id ? (
+                                <div className="content">
+                                    {dataItem.description}
+                                </div>
+                            ) : null
+                        }
                     </div>
-                     </div>)
-                
+                ))
                 : <div>no data found</div>
             }
         </div>
-        
     </div>
     )
 }
