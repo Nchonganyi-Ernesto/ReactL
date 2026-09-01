7603efb6b0907269dce7c0f674f879e09f2d9334
Nchonganyi-Ernesto <nchonganyiernesto4@gmail.com>
2026-09-01 02:22:01 -0700

    color alteration in random manner

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import According from './components/According';
+import Randomcolor from './components/Randomcolor';
 
 function App(){
     return (
         <div className='App'>
          <According/>
+         <Randomcolor/>
         </div>
+        
 
     );
+   
 }
 
 export default App;
